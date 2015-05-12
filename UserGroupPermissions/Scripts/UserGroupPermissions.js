563eb76f3dcf7dce436327de55873b516b06fe76
Carl Jackson <carl.jackson@jaywing.com>
2015-05-12 09:23:50 +0100

    no need for js in umb 7

--- UserGroupPermissions/Scripts/UserGroupPermissions.js
+++ UserGroupPermissions/Scripts/UserGroupPermissions.js
@@ -1,6 +1,8 @@
 ﻿function openUserGroupDialog() 
 {
+    /*
     if (UmbClientMgr.mainTree().getActionNode().nodeId != '-1' && UmbClientMgr.mainTree().getActionNode().nodeType != '') {
         UmbClientMgr.openModalWindow("plugins/usergrouppermissions/dialogs/permissions.aspx?id=" + UmbClientMgr.mainTree().getActionNode().nodeId + '&rnd=', 'Set role permissions', true, 800, 400);
     }
+    */
 }
